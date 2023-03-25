b8786dc7aae17185ce62bc8c5b90615919ce3876
Your Name <you@example.com>
2023-03-25 18:08:46 +0000

    versit

---
+++ src/main/java/com/hamza/jpa_hiebernate_project/JpaHiebernateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hamza.jpa_hiebernate_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHiebernateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHiebernateProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamza/jpa_hiebernate_project/JpaHiebernateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamza.jpa_hiebernate_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHiebernateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
